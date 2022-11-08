b9c0c32216776086968a1ef25caa76dfdb8db12b
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2022-11-08 07:21:48 +0700

    added /api/docs for documentation of this api

--- src/routes.ts
+++ src/routes.ts
@@ -12,9 +12,12 @@ import requireUser from './middlewares/requireUser';
 import { createUserInputSchema } from './schemas/user.schema';
 import { loginInputSchema } from './schemas/auth.schema';
 import validateInput from './utils/validateInput';
+import swaggerUI from 'swagger-ui-express';
+import swaggerDocs from './../swagger.json';
 
 export default function routes(app: Express) {
   app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
+  app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
   app.post(
     '/api/register',
