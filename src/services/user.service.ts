2ebf2123a746ed39e0d5dad29bc2d4dbc96bd081
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2022-11-27 15:17:43 +0700

    - create the username while registering the user - add createUserProfile service to create the user profile

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,19 +1,50 @@
 import { PrismaClient, Prisma } from '@prisma/client';
-import { create } from 'domain';
-import { TCreateUserInput } from '../schemas/user.schema';
 
 const prisma = new PrismaClient();
 
 export async function createUser({
   email,
   password,
+  username,
 }: {
   email: string;
   password: string;
-}): Promise<{ email: string; id: number }> {
+  username: string;
+}): Promise<{
+  email: string;
+  id: number;
+  UserProfile: { username: string; id: number }[];
+}> {
   const user = await prisma.user.create({
-    data: { email, password },
-    select: { email: true, id: true },
+    data: { email, password, UserProfile: { create: { username } } },
+    select: {
+      email: true,
+      id: true,
+      UserProfile: {
+        select: { username: true, id: true },
+        where: { username },
+      },
+    },
   });
   return user;
 }
+
+export async function createUserProfile({
+  userId,
+  username,
+  fullName,
+  bio,
+}: {
+  userId: number;
+  username: string;
+  fullName: string;
+  bio: string;
+}): Promise<{ username: string; fullName: string }> {
+  const result = prisma.userProfile.create({
+    data: { fullName, username, bio, UserId: { connect: { id: userId } } },
+  });
+
+  return result;
+}
+
+export async function updateUserProfile({ bio }: { bio: string }) {}
