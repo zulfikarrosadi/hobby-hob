0f8677931ffbf5cfea64510c2cfe18d4786e4210
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2022-11-27 15:33:42 +0700

    add username and userProfileId to return object

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -6,7 +6,12 @@ const prisma = new PrismaClient();
 export async function findUserByEmail({ email }: TLoginInput['body']) {
   const user = await prisma.user.findUnique({
     where: { email },
-    select: { id: true, email: true, password: true },
+    select: {
+      id: true,
+      email: true,
+      password: true,
+      UserProfile: { select: { id: true, username: true } },
+    },
   });
 
   return user;
