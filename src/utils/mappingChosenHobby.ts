f535bfcf6146e8400af9c50d48fc83a135098326
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2022-11-27 15:22:49 +0700

    add utility funciton to mapping the user input data when adding new hobby

---
+++ src/utils/mappingChosenHobby.ts
@@ -0,0 +1,10 @@
+export default function mapUserHoobiesInput({
+  userId,
+  hobbyIds,
+}: {
+  userId: number;
+  hobbyIds: number[];
+}): { userId: number; hobbyId: number }[] {
+  const uniqueHobbyId = [...new Set(hobbyIds)];
+  return uniqueHobbyId.map((hobbyId) => ({ userId, hobbyId }));
+}
