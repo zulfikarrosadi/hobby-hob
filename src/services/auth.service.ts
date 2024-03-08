a76224f276c675c2f6962c87f7035b088b11907e
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-03-08 10:20:14 +0700

    change the parameter type to satify typescript

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,7 +3,9 @@ import { TLoginInput } from '../schemas/auth.schema';
 
 const prisma = new PrismaClient();
 
-export async function findUserByEmail({ email }: TLoginInput['body']) {
+export async function findUserByEmail({
+  email,
+}: Pick<TLoginInput['body'], 'email'>) {
   const user = await prisma.user.findUnique({
     where: { email },
     select: {
