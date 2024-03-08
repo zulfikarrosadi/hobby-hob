534342d3da411c9d444b60f3821016d56087ebc9
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-03-08 10:25:36 +0700

    forcing all handler res to follow GeneralResponse
    so user can consume this API easily and provide better error handling

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -8,18 +8,16 @@ import {
   createUserProfile,
   getUserProfile,
 } from '../services/user.service';
-import createSession from '../utils/sessoionHelper';
+import createSession, { setExpiredDate } from '../utils/sessoionHelper';
 import { hashPassword, mappingUserHobby } from '../utils/userHelper';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
-import { constraintError } from '../utils/validateAndThrow';
 import { generateUsername } from '../utils/userHelper';
-import { mappingUserHobbyResult } from '../utils/hobbyHelper';
+import GeneralResponse from '../schemas/responses.schema';
 
 export async function createUserHandler(
   req: Request<{}, {}, Required<TCreateUserInput['body']>> & {
     user?: { email: string };
   },
-  res: Response,
+  res: Response<GeneralResponse>,
 ) {
   const { email, password } = req.body;
   try {
@@ -29,6 +27,9 @@ export async function createUserHandler(
       password: hashedPassword,
       username: generateUsername(),
     });
+    if (user instanceof Error) {
+      throw new Error(user.message);
+    }
     const token = createSession({
       email: user.email,
       userId: user.id,
@@ -36,77 +37,102 @@ export async function createUserHandler(
     });
 
     res.cookie('refreshToken', token.get('refreshToken'), {
+      httpOnly: true,
       sameSite: 'none',
-      secure: true,
-      maxAge: 86400,
+      path: '/auth/refresh',
+      expires: setExpiredDate({ day: 15 }),
     });
     res.cookie('accessToken', token.get('accessToken'), {
+      httpOnly: true,
       sameSite: 'none',
-      secure: true,
-      maxAge: 86400,
+      expires: setExpiredDate({ hour: 1 }),
     });
     return res.status(201).json({
-      email: user.email,
-      userId: user.id,
-      userProfileId: user.UserProfile[0].id,
-      username: user.UserProfile[0].username,
-      accessToken: token.get('accessToken'),
-      refreshToken: token.get('refreshToken'),
+      status: 'success',
+      data: {
+        user: {
+          email: user.email,
+          id: user.id,
+          userProfileId: user.UserProfile[0].id,
+          username: user.UserProfile[0].username,
+          accessToken: token.get('accessToken'),
+          refreshToken: token.get('refreshToken'),
+        },
+      },
     });
-  } catch (error) {
+  } catch (error: any) {
     console.log(error);
-    let e: { message: string; code: number };
-
-    if (error instanceof PrismaClientKnownRequestError) {
-      e = constraintError({ prismaErrCode: error.code, meta: error.meta });
-    }
-    return res.status(400).json({ message: e.message });
+    return res.status(400).json({
+      status: 'fail',
+      errors: [{ code: 400, message: error.message }],
+    });
   }
 }
 
 export async function createUserProfileHandler(
   req: Request<{}, {}, Required<TCreateUserProfileIntput['body']>>,
-  res: Response<{}, { user: { userId: number; email: string } }>,
+  res: Response<GeneralResponse, { user: { userId: number; email: string } }>,
 ) {
   try {
     const result = await createUserProfile({
       userId: res.locals.user.userId,
       ...req.body,
     });
-
-    return res.status(201).json({
-      username: result.username,
-      message: 'success',
-    });
-  } catch (error) {
-    let e: { message: string; code: number };
-
-    if (error instanceof PrismaClientKnownRequestError) {
-      e = constraintError({ prismaErrCode: error.code, meta: error.meta });
+    if (result instanceof Error) {
+      throw new Error(result.message);
     }
-
-    return res.status(e.code).json({ message: e.message });
+    return res.status(201).json({
+      status: 'success',
+      data: {
+        user: {
+          id: result.userId,
+          fullName: result.fullName,
+          username: result.username,
+        },
+      },
+    });
+  } catch (error: any) {
+    return res.status(400).json({
+      status: 'fail',
+      errors: [
+        {
+          code: 400,
+          message: error.message,
+        },
+      ],
+    });
   }
 }
 
-export async function getUserProfileHandler(req: Request, res: Response) {
+export async function getUserProfileHandler(
+  req: Request,
+  res: Response<GeneralResponse>,
+) {
   const userProfileId = parseInt(req.params.userProfileId, 10);
-
   try {
     const user = await getUserProfile({ userProfileId });
+    if (user instanceof Error) {
+      throw new Error(user.message);
+    }
     const mappedHobby = mappingUserHobby(user.UserHobby);
-    const result = {
-      id: user.id,
-      username: user.username,
-      fullName: user.fullName,
-      bio: user.bio,
-      sosmed: user.sosmed,
-      hobbies: mappedHobby.hobby,
-    };
 
-    if (!user) throw Error('user does not exists');
-    return res.status(200).json(result);
-  } catch (error) {
-    return res.status(404).json({ message: error.message });
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        user: {
+          id: user.id,
+          username: user.username,
+          fullName: user.fullName,
+          bio: user.bio,
+          sosmed: user.sosmed,
+          hobbies: mappedHobby.hobby,
+        },
+      },
+    });
+  } catch (error: any) {
+    return res.status(404).json({
+      status: 'fail',
+      errors: [{ code: 404, message: error.message }],
+    });
   }
 }
