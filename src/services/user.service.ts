bbec27cc4d5cc77df84a8aa7af57ed3cd0622e76
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-03-08 10:23:16 +0700

    forcing all service to can returning error
    this is done in service level so we can catch the error immediately and provide better error message

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -10,23 +10,36 @@ export async function createUser({
   email: string;
   password: string;
   username: string;
-}): Promise<{
-  email: string;
-  id: number;
-  UserProfile: { username: string; id: number }[];
-}> {
-  const user = await prisma.user.create({
-    data: { email, password, UserProfile: { create: { username } } },
-    select: {
-      email: true,
-      id: true,
-      UserProfile: {
-        select: { username: true, id: true },
-        where: { username },
+}): Promise<
+  | {
+      email: string;
+      id: number;
+      UserProfile: { username: string; id: number }[];
+    }
+  | Error
+> {
+  try {
+    const user = await prisma.user.create({
+      data: { email, password, UserProfile: { create: { username } } },
+      select: {
+        email: true,
+        id: true,
+        UserProfile: {
+          select: { username: true, id: true },
+          where: { username },
+        },
       },
-    },
-  });
-  return user;
+    });
+    return user;
+  } catch (error: any) {
+    console.log(error);
+    if (error.code === 'P2002') {
+      return new Error(
+        'The email address you entered is already in use. Please try a different email address',
+      );
+    }
+    return new Error('cannot create user, please try again');
+  }
 }
 
 export async function createUserProfile({
@@ -47,19 +60,29 @@ export async function createUserProfile({
     website?: string;
     others?: string[];
   };
-}): Promise<{ username: string; fullName: string }> {
-  const result = await prisma.userProfile.update({
-    data: {
-      fullName,
-      username,
-      bio,
-      sosmed,
-      UserId: { connect: { id: userId } },
-    },
-    where: { userId },
-  });
+}): Promise<{ username: string; fullName: string; userId: number } | Error> {
+  try {
+    const result = await prisma.userProfile.update({
+      data: {
+        fullName,
+        username,
+        bio,
+        sosmed,
+        UserId: { connect: { id: userId } },
+      },
+      select: { userId: true, username: true, fullName: true },
+      where: { userId },
+    });
 
-  return result;
+    return {
+      fullName: result.fullName ?? '',
+      userId: result.userId,
+      username: result.username,
+    };
+  } catch (error: any) {
+    console.log(error);
+    return new Error("there's something wrong, please try again");
+  }
 }
 
 export async function updateUserProfile({ bio }: { bio: string }) {}
@@ -68,24 +91,42 @@ export async function getUserProfile({
   userProfileId,
 }: {
   userProfileId: number;
-}): Promise<{
-  id: number;
-  username: string;
-  fullName: string;
-  bio: string;
-  sosmed: Prisma.JsonValue;
-  UserHobby: {
-    userId: number;
-    hobbyId: number;
-    hobby: { id: number; name: string };
-  }[];
-}> {
-  const result = await prisma.userProfile.findFirst({
-    where: { id: userProfileId },
-    include: {
-      UserHobby: { include: { hobby: { select: { id: true, name: true } } } },
-    },
-  });
-
-  return result;
+}): Promise<
+  | {
+      id: number;
+      username: string;
+      fullName: string;
+      bio: string;
+      sosmed: Prisma.JsonValue;
+      UserHobby: {
+        userId: number;
+        hobbyId: number;
+        hobby: { id: number; name: string };
+      }[];
+    }
+  | Error
+> {
+  try {
+    const result = await prisma.userProfile.findFirst({
+      where: { id: userProfileId },
+      include: {
+        UserHobby: { include: { hobby: { select: { id: true, name: true } } } },
+      },
+    });
+    if (result instanceof Error || !result) {
+      console.log(result);
+      throw new Error('users not found, please try again');
+    }
+    return {
+      id: result.id,
+      username: result.username,
+      fullName: result.fullName ?? '',
+      bio: result.bio ?? '',
+      UserHobby: result.UserHobby,
+      sosmed: result.sosmed,
+    };
+  } catch (error: any) {
+    console.log(error);
+    return error;
+  }
 }
