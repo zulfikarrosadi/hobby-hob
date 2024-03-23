89743b4f85c65223f5a897806e50b914233bd38e
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-03-23 10:50:03 +0700

    construct validation error response following response.schema

--- src/utils/validateAndThrow.ts
+++ src/utils/validateAndThrow.ts
@@ -1,13 +1,31 @@
 import { NextFunction, Request, Response } from 'express';
 import { AnyZodObject } from 'zod';
+import GeneralResponse from '../schemas/responses.schema';
 
 export function validateInput(schema: AnyZodObject) {
-  return function (req: Request, res: Response, next: NextFunction) {
+  return function (
+    req: Request,
+    res: Response<GeneralResponse>,
+    next: NextFunction,
+  ) {
     try {
-      schema.parse({ body: req.body });
+      schema.parse(req.body);
       return next();
-    } catch (error) {
-      return res.status(400).send(error.errors);
+    } catch (error: any) {
+      console.log(error);
+      return res.status(400).send({
+        status: 'fail',
+        errors: {
+          message: 'validation errors',
+          code: 400,
+          details: error.errors.map((e: any) => {
+            return {
+              message: e.message,
+              path: e.path[0],
+            };
+          }),
+        },
+      });
     }
   };
 }
