baaccedddf62e998d5e367c2926c440176e5e907
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-04-07 22:11:43 +0700

    enforcing all response to have the same property

--- src/controllers/hobby.controller.ts
+++ src/controllers/hobby.controller.ts
@@ -16,123 +16,156 @@ import GeneralResponse from '../schemas/responses.schema';
 
 export async function getHobbiesHandler(
   req: Request<{}, {}, {}, { hobbyName: string }>,
-  res: Response,
+  res: Response<GeneralResponse>,
 ) {
   const { hobbyName } = req.query;
 
   try {
     if (hobbyName) {
       const hobbies = await findHobbyByName({ hobbyName });
-      if (!hobbies) throw Error('hobby not found');
-
+      if (hobbies instanceof Error) {
+        throw new Error('hobby not found');
+      }
       return res.status(200).json({
-        message: 'success',
-        data: hobbies,
+        status: 'success',
+        data: { hobbies },
       });
     }
     const hobbies = await getHobbies();
 
     return res.status(200).json({
-      message: 'success',
-      cursor: hobbies[hobbies.length - 1].name,
-      data: hobbies,
+      status: 'success',
+      data: { cursor: hobbies[hobbies.length - 1].id, hobbies },
     });
   } catch (error) {
-    return res.status(404).json({ message: 'hobby not found' });
+    console.log('catching error', error);
+    return res.status(404).json({
+      status: 'fail',
+      errors: { code: 404, message: 'hobby not found' },
+    });
   }
 }
 
 export async function addHobbyToUserHandler(
-  req: Request<{}, {}, Required<TCreateUserHobbyInput['body']>>,
-  res: Response,
+  req: Request<{}, {}, Required<TCreateUserHobbyInput>>,
+  res: Response<GeneralResponse>,
 ) {
   try {
-    const data = mapUserHoobiesInput({
+    const userIdAndHoobyId = mapUserHoobiesInput({
       userId: res.locals.user.userProfileId,
       hobbyIds: req.body.hobbyId,
     });
 
-    await connectHobbiesToUser(data);
-
-    return res.status(201).json({ message: 'success' });
-  } catch (error) {
+    const result = await connectHobbiesToUser(userIdAndHoobyId);
+    if (result.count < 1) {
+      throw new Error('maybe you already join this hobby community');
+    }
+    return res.status(201).json({ status: 'success' });
+  } catch (error: any) {
     console.log(error);
-    return res.status(400).json(error);
+    return res
+      .status(400)
+      .json({ status: 'fail', errors: { code: 400, message: error.message } });
   }
 }
 
 export async function getInfiniteHobbiesHandler(
   req: Request<{ cursor: string }>,
-  res: Response,
+  res: Response<GeneralResponse>,
 ) {
   const { cursor } = req.params;
+  const hobbyId = parseInt(cursor, 10);
 
   try {
-    const hobbies = await getInfiniteHobbies({ name: cursor });
-    console.log(hobbies);
+    const hobbies = await getInfiniteHobbies({ id: hobbyId });
+    if (!hobbies.length) {
+      throw new Error(
+        "Currently there're no more hobbies can be found. I guess, that's the end of it",
+      );
+    }
 
     return res.status(200).json({
-      message: 'success',
-      cursor: hobbies[hobbies.length - 1].name,
-      data: hobbies,
+      status: 'success',
+      data: {
+        cursor: hobbies[hobbies.length - 1].id,
+        hobbies,
+      },
     });
-  } catch (error) {
+  } catch (error: any) {
     console.log(error);
-    return res.status(404).json({ message: 'hobby not found' });
+    return res.status(404).json({
+      status: 'fail',
+      errors: {
+        code: 404,
+        message: error.message,
+      },
+    });
   }
 }
 
 export async function getInfiniteHobbyAndUserHandler(
-  req: Request<{ hobbyName: string; hobbyId: string; userProfileId: string }>,
-  res: Response,
+  req: Request<{ hobbyId: string; userProfileId: string }>,
+  res: Response<GeneralResponse>,
 ) {
-  const { hobbyName } = req.params;
   const hobbyId = parseInt(req.params.hobbyId, 10);
   const userProfileId = parseInt(req.params.userProfileId, 10);
 
   try {
-    const rawData = await getInfiniteHobbyAndUser({
-      hobbyId,
-      hobbyName,
-      userProfileId,
-    });
+    const result = await getInfiniteHobbyAndUser({ hobbyId, userProfileId });
 
-    const result = mappingUserHobbyResult(rawData);
+    if (result instanceof Error) {
+      throw new Error(result.message);
+    }
 
     return res.status(200).json({
-      message: 'success',
-      cursor: {
-        hobbyId: result.hobbyId,
-        hobbyName: result.hobbyName,
-        userProfileId: result.users[result.users.length - 1].id,
+      status: 'success',
+      data: {
+        cursor: {
+          hobbyId: result.hobbyId,
+          userProfileId: result.users[result.users.length - 1].id,
+        },
+        users: result.users,
       },
-      data: result,
     });
-  } catch (error) {
+  } catch (error: any) {
     console.log(error);
-    return res.status(404).json({ message: 'User not found' });
+    return res
+      .status(404)
+      .json({ status: 'fail', errors: { code: 404, message: error.message } });
   }
 }
 
-export async function getHobbyAndUserHandler(req: Request, res: Response) {
+export async function getHobbyAndUserHandler(
+  req: Request,
+  res: Response<GeneralResponse>,
+) {
   const hobbyId = parseInt(req.params.hobbyId, 10);
   try {
-    const rawData = await getUserAndHobby({ id: hobbyId });
-
-    const result = mappingUserHobbyResult(rawData);
+    const result = await getUserAndHobby({ id: hobbyId });
+    if (result instanceof Error) {
+      throw new Error(result.message);
+    }
 
     return res.status(200).json({
-      message: 'success',
-      cursor: {
-        hobbyId: result.hobbyId,
-        hobbyName: result.hobbyName,
-        userProfileId: result.users[result.users.length - 1].id,
+      status: 'success',
+      data: {
+        cursor: {
+          userProfileId: result.users[result.users.length - 1].id,
+        },
+        hobby: {
+          id: result.hobbyId,
+          name: result.hobbyName,
+          description: result.hobbyDescription,
+          image: result.hobbyImage,
+        },
+        users: result.users,
       },
-      data: result,
     });
-  } catch (error) {
+  } catch (error: any) {
     console.log(error);
 
-    return res.status(400).json({ message: 'user not found' });
+    return res
+      .status(404)
+      .json({ status: 'fail', errors: { code: 404, message: error.message } });
   }
 }
