70a7bbfcf70634ff4063954479ca9ef9af7e3f38
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-04-09 05:30:58 +0700

    add createBlog function

--- src/services/blog.service.ts
+++ src/services/blog.service.ts
@@ -1,3 +1,23 @@
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+
+export async function createBlog(blog: {
+  title: string;
+  content: string;
+  userProfileId: number;
+  hobbyId: number;
+}) {
+  const result = await prisma.blog.create({
+    data: {
+      title: blog.title,
+      content: blog.content,
+      userId: blog.userProfileId,
+      hobbyId: blog.hobbyId,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return result;
+}
