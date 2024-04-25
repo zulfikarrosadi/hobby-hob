1f2522cc6f99afaa394cf374a44db8a1cf3ac1da
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-04-25 21:25:19 +0700

    add updatePostSchema zod validation

--- src/schemas/post.schema.ts
+++ src/schemas/post.schema.ts
@@ -7,6 +7,12 @@ export const createPostSchema = z.object({
   hobbyId: z.number({ required_error: 'hobbyId is required' }),
 });
 
+export const updatePostSchema = z.object({
+  content: z
+    .string({ required_error: 'content is required' })
+    .min(1, 'content is required'),
+  hobbyId: z.number({ required_error: 'hobbyId is required' }),
 });
 
 export type PostSchema = z.TypeOf<typeof createPostSchema>;
+export type UpdatePostSchema = z.TypeOf<typeof updatePostSchema>;
