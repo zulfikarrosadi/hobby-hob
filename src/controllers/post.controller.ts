28e6b2e44d058b29d3f510c3d6a1773968d52429
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-04-25 21:30:58 +0700

    add getPosts handler
    this handler is to use to retrive all posts from hobby

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,7 +1,35 @@
 import { Request, Response } from 'express';
-import { savePost } from '../services/post.service';
-import { PostSchema } from '../schemas/post.schema';
+import {
+  deletePostById,
+  getPostsInHobby,
+  savePost,
+  updatePostById,
+} from '../services/post.service';
+import { PostSchema, UpdatePostSchema } from '../schemas/post.schema';
 import ApiResponse from '../schemas/responses.schema';
+
+export async function getPosts(
+  req: Request<{ hobbyId: string }>,
+  res: Response<ApiResponse>,
+) {
+  const posts = await getPostsInHobby({ id: parseInt(req.params.hobbyId, 10) });
+
+  return res.status(200).json({
+    status: 'success',
+    data: {
+      posts: posts?.Post.map((post) => {
+        return {
+          id: post.id,
+          content: post.content,
+          user: {
+            id: post.User.id,
+            username: post.User.username,
+          },
+        };
+      }),
+    },
+  });
+}
 
 export async function createPost(
   req: Request<{}, {}, PostSchema>,
