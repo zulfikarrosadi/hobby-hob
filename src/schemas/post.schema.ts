bd81c3cd56bcebcc5dd5a882aaade129b4e36401
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-04-25 21:24:43 +0700

    change hobbyId type to number when create new post

--- src/schemas/post.schema.ts
+++ src/schemas/post.schema.ts
@@ -4,7 +4,9 @@ export const createPostSchema = z.object({
   content: z
     .string({ required_error: 'content is required' })
     .min(1, 'content is required'),
-  hobbyId: z.string({ required_error: 'hobbyId is required' }),
+  hobbyId: z.number({ required_error: 'hobbyId is required' }),
+});
+
 });
 
 export type PostSchema = z.TypeOf<typeof createPostSchema>;
