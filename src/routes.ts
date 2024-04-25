c3f9b610c47995cc9ca731f162a78d18e1857b17
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-04-25 21:37:37 +0700

    import getHobbyByIdHandler

--- src/routes.ts
+++ src/routes.ts
@@ -24,6 +24,7 @@ import {
   addHobbyToUserHandler,
   getHobbiesHandler,
   getHobbyAndUserHandler,
+  getHobbyByIdHandler,
   getInfiniteHobbiesHandler,
   getInfiniteHobbyAndUserHandler,
 } from './controllers/hobby.controller';
